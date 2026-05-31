bf25b162fe154b0bdc3b6e5aa124a3be30ae1385
nikkiayala29-cloud <nikkiayala29@gmail.com>
2026-05-31 11:53:16 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,27 +1,32 @@
-const restaurants = [
+const foods = [
   {
-    name: "Burger House",
-    food: [
-      {
-        name: "Cheeseburger",
-        price: 120,
-        img: "assets/images/burger.jpg"
-      },
-      {
-        name: "Double Burger",
-        price: 180,
-        img: "assets/images/double.jpg"
-      }
-    ]
+    name: "Cheeseburger",
+    price: 120,
+    img: "assets/images/burger.jpg",
+    desc: "Juicy beef burger with cheese"
   },
   {
-    name: "Pizza Palace",
-    food: [
-      {
-        name: "Pepperoni Pizza",
-        price: 250,
-        img: "assets/images/pizza.jpg"
-      }
-    ]
+    name: "Pepperoni Pizza",
+    price: 250,
+    img: "assets/images/pizza.jpg",
+    desc: "Classic cheesy pepperoni pizza"
+  },
+  {
+    name: "French Fries",
+    price: 90,
+    img: "assets/images/fries.jpg",
+    desc: "Crispy golden fries"
+  },
+  {
+    name: "Fried Chicken",
+    price: 150,
+    img: "assets/images/chicken.jpg",
+    desc: "Crispy fried chicken"
+  },
+  {
+    name: "Chocolate Cake",
+    price: 130,
+    img: "assets/images/dessert.jpg",
+    desc: "Sweet chocolate dessert"
   }
 ];
