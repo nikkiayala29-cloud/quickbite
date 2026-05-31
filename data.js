748e3b3bd6989afb7203c4478ae42b8be65016f9
nikkiayala29-cloud <nikkiayala29@gmail.com>
2026-05-31 11:40:48 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,27 @@
-
+const restaurants = [
+  {
+    name: "Burger House",
+    food: [
+      {
+        name: "Cheeseburger",
+        price: 120,
+        img: "assets/images/burger.jpg"
+      },
+      {
+        name: "Double Burger",
+        price: 180,
+        img: "assets/images/double.jpg"
+      }
+    ]
+  },
+  {
+    name: "Pizza Palace",
+    food: [
+      {
+        name: "Pepperoni Pizza",
+        price: 250,
+        img: "assets/images/pizza.jpg"
+      }
+    ]
+  }
+];
