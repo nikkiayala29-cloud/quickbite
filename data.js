91ca7882c0ff599dc53ce65f89df7d56f9cb078a
nikkiayala29-cloud <nikkiayala29@gmail.com>
2026-05-31 11:56:03 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,32 +1,7 @@
 const foods = [
-  {
-    name: "Cheeseburger",
-    price: 120,
-    img: "assets/images/burger.jpg",
-    desc: "Juicy beef burger with cheese"
-  },
-  {
-    name: "Pepperoni Pizza",
-    price: 250,
-    img: "assets/images/pizza.jpg",
-    desc: "Classic cheesy pepperoni pizza"
-  },
-  {
-    name: "French Fries",
-    price: 90,
-    img: "assets/images/fries.jpg",
-    desc: "Crispy golden fries"
-  },
-  {
-    name: "Fried Chicken",
-    price: 150,
-    img: "assets/images/chicken.jpg",
-    desc: "Crispy fried chicken"
-  },
-  {
-    name: "Chocolate Cake",
-    price: 130,
-    img: "assets/images/dessert.jpg",
-    desc: "Sweet chocolate dessert"
-  }
+  { id: 1, name: "Cheeseburger", price: 120, category: "Burger", img: "assets/images/burger.jpg" },
+  { id: 2, name: "Pepperoni Pizza", price: 250, category: "Pizza", img: "assets/images/pizza.jpg" },
+  { id: 3, name: "French Fries", price: 90, category: "Snack", img: "assets/images/fries.jpg" },
+  { id: 4, name: "Fried Chicken", price: 150, category: "Chicken", img: "assets/images/chicken.jpg" },
+  { id: 5, name: "Chocolate Cake", price: 130, category: "Dessert", img: "assets/images/cake.jpg" }
 ];
